84b40a247a3c3105d698b21b55b616a0f6de6bff
auxves <me@auxves.dev>
2024-06-25 08:44:07 -0700

    make traefik internal

--- hosts/tide/services/traefik.nix
+++ hosts/tide/services/traefik.nix
@@ -1,7 +1,7 @@
-{ ... }: {
+{ pkgs, ... }: {
   disko.devices.zpool.storage.datasets."services/traefik".type = "zfs_fs";
 
-  virtualisation.oci-containers.containers."traefik" = {
+  virtualisation.oci-containers.containers.traefik-internal = {
     image = "traefik:v3.0.0@sha256:7996bdae8aaa70eaacf2978b6c949de5b68c0a24ddc3e40c06344ecc88cfaea3";
     autoStart = true;
 
@@ -16,6 +16,7 @@
 
     extraOptions = [
       "--network=lan"
+      "--network=traefik-internal"
       "--ip6=2600:1700:78c0:130f:abcd::1"
       "--secret=traefik_cloudflare_token"
     ];
@@ -44,5 +45,22 @@
       "traefik.http.routers.traefik.service" = "api@internal";
       "traefik.http.services.traefik.loadbalancer.server.port" = "9999";
     };
+  };
+
+  systemd.services."podman-network-traefik-internal" = {
+    path = [ pkgs.podman ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStop = "${pkgs.podman}/bin/podman network rm -f traefik-internal";
+    };
+
+    script = ''
+      podman network exists traefik-internal || podman network create traefik-internal --ipv6 --disable-dns
+    '';
+
+    partOf = [ "podman-networks.target" ];
+    wantedBy = [ "podman-networks.target" ];
   };
 }
