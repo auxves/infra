7e5fcc75a31525a08e97ecc5e4b8d7fcb82977ef
auxves <me@auxves.dev>
2024-06-26 08:31:14 -0700

    specify host port for dns

--- hosts/tide/services/dns.nix
+++ hosts/tide/services/dns.nix
@@ -5,7 +5,7 @@
     image = "jpillora/dnsmasq@sha256:98b69ad825942089fb7c4b9153e3c5af0205eda3a103c691e30b1a13fd912830";
     autoStart = true;
 
-    ports = [ "[fd7a:115c:a1e0::4d01:292e]::53" ];
+    ports = [ "[fd7a:115c:a1e0::4d01:292e]:53:53" ];
 
     volumes = [
       "/storage/services/dns/dnsmasq.conf:/etc/dnsmasq.conf"
