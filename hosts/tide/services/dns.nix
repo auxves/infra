05790817669d4ca76ee112b3776caf72b8f8eb05
auxves <me@auxves.dev>
2024-06-26 08:28:44 -0700

    move away from macvlan

--- hosts/tide/services/dns.nix
+++ hosts/tide/services/dns.nix
@@ -5,12 +5,10 @@
     image = "jpillora/dnsmasq@sha256:98b69ad825942089fb7c4b9153e3c5af0205eda3a103c691e30b1a13fd912830";
     autoStart = true;
 
+    ports = [ "[fd7a:115c:a1e0::4d01:292e]::53" ];
+
     volumes = [
       "/storage/services/dns/dnsmasq.conf:/etc/dnsmasq.conf"
-    ];
-
-    extraOptions = [
-      "--network=lan:ip6=2600:1700:78c0:130f:abcd::4a0c,mac=62:c1:5b:7f:15:38"
     ];
   };
 }
