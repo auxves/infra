35facdb7733d8111c7284e9e5d6f6917a11249c6
auxves <me@auxves.dev>
2024-07-01 17:55:59 -0700

    specify dnsmasq tag

--- hosts/tide/services/dns.nix
+++ hosts/tide/services/dns.nix
@@ -2,7 +2,7 @@
   disko.devices.zpool.storage.datasets."services/dns".type = "zfs_fs";
 
   virtualisation.oci-containers.containers.dnsmasq = {
-    image = "jpillora/dnsmasq@sha256:98b69ad825942089fb7c4b9153e3c5af0205eda3a103c691e30b1a13fd912830";
+    image = "jpillora/dnsmasq:latest@sha256:98b69ad825942089fb7c4b9153e3c5af0205eda3a103c691e30b1a13fd912830";
     autoStart = true;
 
     ports = [
