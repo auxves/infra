c8d7ba1ac76282740cca0b86ebb9a5236aae401c
auxves <me@auxves.dev>
2024-07-21 16:16:48 -0700

    add zitadel

---
+++ hosts/tide/services/zitadel/default.nix
@@ -0,0 +1,63 @@
+{ config, ... }:
+let
+  host = "auth.auxves.dev";
+
+  paths = config.storage.paths;
+in
+{
+  storage.paths."services/zitadel" = { };
+  sops.secrets."zitadel/env" = { };
+
+  virtualisation.oci-containers.containers.zitadel = {
+    image = "ghcr.io/zitadel/zitadel:v2.55.3@sha256:22b23fa7bd73dab827b5df4d5f7089637d42f4c4c93c20536290aaf9f2a016ce";
+
+    environmentFiles = [ config.sops.secrets."zitadel/env".path ];
+
+    environment = {
+      ZITADEL_DATABASE_POSTGRES_HOST = "zitadel-postgres";
+      ZITADEL_DATABASE_POSTGRES_PORT = "5432";
+      ZITADEL_DATABASE_POSTGRES_DATABASE = "zitadel";
+      ZITADEL_DATABASE_POSTGRES_USER_USERNAME = "zitadel";
+      ZITADEL_DATABASE_POSTGRES_USER_SSL_MODE = "disable";
+      ZITADEL_DATABASE_POSTGRES_ADMIN_USERNAME = "postgres";
+      ZITADEL_DATABASE_POSTGRES_ADMIN_SSL_MODE = "disable";
+      ZITADEL_EXTERNALSECURE = "true";
+      ZITADEL_EXTERNALDOMAIN = host;
+      ZITADEL_EXTERNALPORT = "443";
+    };
+
+    cmd = [ "start-from-init" "--masterkeyFromEnv" "--tlsMode=external" ];
+
+    dependsOn = [ "zitadel-postgres" ];
+
+    labels = {
+      "traefik.enable" = "true";
+      "traefik.http.routers.zitadel.rule" = "Host(`${host}`)";
+      "traefik.http.routers.zitadel.entrypoints" = "public";
+      "traefik.http.services.zitadel.loadbalancer.server.port" = "8080";
+    };
+  };
+
+  storage.paths."services/zitadel/postgres" = { };
+  sops.secrets."zitadel/postgres/env" = { };
+
+  virtualisation.oci-containers.containers.zitadel-postgres = {
+    image = "postgres:16-alpine@sha256:de3d7b6e4b5b3fe899e997579d6dfe95a99539d154abe03f0b6839133ed05065";
+
+    volumes = [
+      "${paths."services/zitadel/postgres".path}:/var/lib/postgresql/data"
+    ];
+
+    environmentFiles = [ config.sops.secrets."zitadel/postgres/env".path ];
+
+    environment = {
+      POSTGRES_USER = "postgres";
+      POSTGRES_DB = "zitadel";
+    };
+
+    extraOptions = [
+      "--health-cmd=pg_isready"
+      "--health-on-failure=stop"
+    ];
+  };
+}
