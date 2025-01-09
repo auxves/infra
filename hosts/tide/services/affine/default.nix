3d9bb45725d4c7c78eeae68bfbf89d68f5ede175
auxves <me@auxves.dev>
2025-01-09 15:22:47 -0800

    tide: add affine

---
+++ hosts/tide/services/affine/default.nix
@@ -0,0 +1,73 @@
+{ config, ... }:
+let
+  paths = config.storage.paths;
+in
+{
+  storage.paths."services/affine" = { };
+  sops.secrets."affine/env" = { };
+
+  virtualisation.oci-containers.containers.affine = {
+    image = "ghcr.io/toeverything/affine-graphql:stable@sha256:08ede007df3d634b0abdbf9afeabfe939137e2888c803c990be57cbdb522b19e";
+
+    volumes = [
+      "${paths."services/affine".path}:/root/.affine"
+    ];
+
+    environmentFiles = [ config.sops.secrets."affine/env".path ];
+
+    environment = {
+      AFFINE_SERVER_EXTERNAL_URL = "https://affine.x.auxves.dev";
+      REDIS_SERVER_HOST = "affine-redis";
+
+      OAUTH_OIDC_SCOPE = "openid email profile offline_access";
+      OAUTH_OIDC_CLAIM_MAP_ID = "preferred_username";
+      OAUTH_OIDC_CLAIM_MAP_EMAIL = "email";
+    };
+
+    cmd = [
+      "sh"
+      "-c"
+      "node ./scripts/self-host-predeploy.js && node --import ./scripts/register.js ./dist/index.js"
+    ];
+
+    dependsOn = [ "affine-redis" "affine-postgres" ];
+
+    labels = {
+      "traefik.enable" = "true";
+      "traefik.http.routers.affine.rule" = "Host(`affine.x.auxves.dev`)";
+      "traefik.http.services.affine.loadbalancer.server.port" = "3010";
+    };
+  };
+
+  virtualisation.oci-containers.containers.affine-redis = {
+    image = "redis:7.2.5@sha256:fb534a36ac2034a6374933467d971fbcbfa5d213805507f560d564851a720355";
+
+    extraOptions = [
+      "--health-cmd=redis-cli ping"
+      "--health-on-failure=stop"
+    ];
+  };
+
+  storage.paths."services/affine/postgres" = { };
+  sops.secrets."affine/postgres/env" = { };
+
+  virtualisation.oci-containers.containers.affine-postgres = {
+    image = "postgres:16-alpine@sha256:de3d7b6e4b5b3fe899e997579d6dfe95a99539d154abe03f0b6839133ed05065";
+
+    volumes = [
+      "${paths."services/affine/postgres".path}:/var/lib/postgresql/data"
+    ];
+
+    environmentFiles = [ config.sops.secrets."affine/postgres/env".path ];
+
+    environment = {
+      POSTGRES_USER = "postgres";
+      POSTGRES_DB = "affine";
+    };
+
+    extraOptions = [
+      "--health-cmd=pg_isready -d \${POSTGRES_USER} -U \${POSTGRES_DB}"
+      "--health-on-failure=stop"
+    ];
+  };
+}
