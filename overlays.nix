45cc0d40201ecf05fbfb12e7bd1f464255f94a63
auxves <me@auxves.dev>
2025-02-20 11:17:15 -0800

    fix(podman): show rfc3339nano in logs

--- overlays.nix
+++ overlays.nix
@@ -8,8 +8,16 @@ rec {
   all = lib.composeManyExtensions [
     packages
     tools
+    patches
     inputs.fenix.overlays.default
   ];
+
+  patches = final: prev: {
+    podman = prev.podman.overrideAttrs
+      (old: {
+        patches = (old.patches or [ ]) ++ [ ./derivations/patches/podman/rfc3339nano-logs.patch ];
+      });
+  };
 
   packages = final: prev: lib.mapAttrs
     (name: _: lib.callPackageWith (final // args // { inherit prev; self = args; }) ./derivations/pkgs/${name} { })
