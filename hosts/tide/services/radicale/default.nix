acae631abe47dcee4fbe91d2135169efbba9d060
auxves <me@auxves.dev>
2025-05-04 00:23:23 -0700

    feat: add radicale

---
+++ hosts/tide/services/radicale/default.nix
@@ -0,0 +1,40 @@
+{ config, ... }:
+let
+  cfg = config.apps.radicale;
+in
+{
+  apps.radicale = {
+    volumes = {
+      radicale = { type = "zfs"; };
+    };
+
+    containers = {
+      radicale = {
+        image = "tomsquest/docker-radicale:3.5.2.0@sha256:7233c1bc2c822f4567c2715406c3f47465188ff864ea653615b6649d194e31b2";
+        volumes = [
+          "${cfg.volumes.radicale.path}:/data"
+          "${./radicale.conf}:/config/config"
+        ];
+      };
+    };
+
+    ingresses = {
+      app = {
+        domain = "radicale.auxves.dev";
+        container = "radicale";
+        port = 5232;
+      };
+    };
+  };
+
+  monitoring.checks = [{
+    name = "radicale";
+    group = "services";
+    url = "https://${cfg.ingresses.app.domain}";
+    interval = "1m";
+    alerts = [{ type = "discord"; }];
+    conditions = [
+      "[STATUS] == 200"
+    ];
+  }];
+}
