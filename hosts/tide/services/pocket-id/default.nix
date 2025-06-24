ee492a70e6d36148eb11736b8a49af69a464ba96
auxves <me@auxves.dev>
2025-06-24 12:12:51 +0400

    feat: add pocket-id

---
+++ hosts/tide/services/pocket-id/default.nix
@@ -0,0 +1,48 @@
+{ config, ... }:
+let
+  cfg = config.apps.pocket-id;
+in
+{
+  apps.pocket-id = {
+    volumes = {
+      data = { type = "zfs"; };
+    };
+
+    containers = {
+      pocket-id = {
+        image = "ghcr.io/pocket-id/pocket-id:v1.4.1@sha256:e01ee742f4c90f0cf1892f48b241ba8a96aa93e7174e8486d76eb4c103abfdff";
+
+        volumes = [
+          "${cfg.volumes.data.path}:/app/data"
+        ];
+
+        environment = {
+          APP_URL = "https://${cfg.ingresses.app.domain}";
+          TRUST_PROXY = "true";
+          PUID = "0";
+          PGID = "0";
+        };
+      };
+    };
+
+    ingresses = {
+      app = {
+        type = "public";
+        domain = "id.auxves.dev";
+        container = "pocket-id";
+        port = 1411;
+      };
+    };
+  };
+
+  monitoring.checks = [{
+    name = "pocket-id";
+    group = "services";
+    url = "https://${cfg.ingresses.app.domain}";
+    interval = "1m";
+    alerts = [{ type = "discord"; }];
+    conditions = [
+      "[STATUS] == 200"
+    ];
+  }];
+}
