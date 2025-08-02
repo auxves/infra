884933802c9dc06659fca618e337a900e2741b10
auxves <me@auxves.dev>
2025-08-01 17:06:11 -0700

    fix(tide): set sysctls for performance

--- hosts/tide/hardware.nix
+++ hosts/tide/hardware.nix
@@ -14,6 +14,17 @@
     kernelParams = [ "i915.modeset=0" "xe.force_probe=a7a1" ];
 
     blacklistedKernelModules = [ "i915" ];
+
+    kernel.sysctl = {
+      "net.core.default_qdisc" = "fq";
+      "net.ipv4.tcp_congestion_control" = "bbr";
+      "net.core.rmem_max" = 16777216;
+      "net.core.wmem_max" = 16777216;
+      "net.ipv4.tcp_rmem" = "4096 87380 16777216";
+      "net.ipv4.tcp_wmem" = "4096 65536 16777216";
+
+      "fs.inotify.max_user_instances" = "8192";
+    };
   };
 
   hardware.enableRedistributableFirmware = true;
