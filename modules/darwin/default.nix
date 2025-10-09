3428ae4cecbeea1909c757823e7c385ca07e8cab
auxves <me@auxves.dev>
2025-10-08 18:20:15 -0700

    darwin: remove temporary fish path fix

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -7,8 +7,6 @@
   users.knownUsers = lib.mkForce [ ];
   users.knownGroups = lib.mkForce [ ];
 
-  programs.fish.loginShellInit = "fish_add_path --move --prepend --path $HOME/.local/bin $HOME/.nix-profile/bin /etc/profiles/per-user/$USER/bin /run/current-system/sw/bin /nix/var/nix/profiles/default/bin";
-
   security.pam.services.sudo_local.touchIdAuth = true;
 
   system.stateVersion = 4;
