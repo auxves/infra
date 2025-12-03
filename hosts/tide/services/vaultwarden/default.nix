8f0393be79f1ef9e8cd83455effcefdbd4377d34
auxves <me@auxves.dev>
2025-12-03 15:13:28 -0800

    vaultwarden: init

---
+++ hosts/tide/services/vaultwarden/default.nix
@@ -0,0 +1,55 @@
+{ config, ... }:
+let
+  cfg = config.apps.vaultwarden;
+in
+{
+  sops.secrets."vaultwarden/env" = { };
+
+  apps.vaultwarden = {
+    volumes = {
+      data = { type = "zfs"; };
+    };
+
+    containers = {
+      vaultwarden = {
+        image = "vaultwarden/server:testing-alpine@sha256:c72cb86d394a40917bb0f6d89a31db70e764dfe83cd54bbe86fc28abd439ea7f";
+
+        volumes = [
+          "${cfg.volumes.data.path}:/data"
+        ];
+
+        environment = {
+          DOMAIN = "https://${cfg.ingresses.app.domain}";
+
+          SIGNUPS_ALLOWED = "false";
+
+          SSO_ENABLED = "true";
+          SSO_ONLY = "true";
+          SSO_AUTHORITY = "https://${config.apps.pocket-id.ingresses.app.domain}";
+          SSO_SCOPES = "email profile groups offline_access";
+        };
+
+        environmentFiles = [ config.sops.secrets."vaultwarden/env".path ];
+      };
+    };
+
+    ingresses = {
+      app = {
+        domain = "bitwarden.auxves.dev";
+        container = "vaultwarden";
+        port = 80;
+      };
+    };
+  };
+
+  monitoring.checks = [{
+    name = "vaultwarden";
+    group = "services";
+    url = "https://${cfg.ingresses.app.domain}";
+    interval = "1m";
+    alerts = [{ type = "discord"; }];
+    conditions = [
+      "[STATUS] == 200"
+    ];
+  }];
+}
