44e3ef6eaab0597f54c44e1ef8378f7b71086d6a
auxves <me@auxves.dev>
2026-05-07 14:53:49 -0700

    security: mitigate dirty frag

--- modules/nixos/security.nix
+++ modules/nixos/security.nix
@@ -1,11 +1,11 @@
 { ... }: {
-  # CVE-2026-31431
-
-  boot.blacklistedKernelModules = [
-    "algif_aead"
-  ];
-
   boot.extraModprobeConfig = ''
+    # Copy Fail (CVE-2026-31431)
     install algif_aead /run/current-system/sw/bin/false
+
+    # Dirty Frag
+    install esp4 /run/current-system/sw/bin/false
+    install esp6 /run/current-system/sw/bin/false
+    install rxrpc /run/current-system/sw/bin/false
   '';
 }
