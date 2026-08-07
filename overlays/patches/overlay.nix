4e910c502876b91b2996477ce37bcb6121f8ad55
auxves <me@auxves.dev>
2026-08-06 18:34:48 -0700

    sanoid: use official nixpkgs version

--- overlays/patches/overlay.nix
+++ overlays/patches/overlay.nix
@@ -1,19 +1,3 @@
 final: prev: {
-  auxves.checks = prev.auxves.checks or { } // prev.lib.internal.prefixAttrs "patches" {
-    inherit (final) sanoid;
-  };
-
-  sanoid = prev.sanoid.overrideAttrs (old: {
-    version = "unstable";
-    src = prev.fetchFromGitHub {
-      owner = "jimsalterjrs";
-      repo = old.pname;
-      rev = "826d4d1c075e3310ec3c1d0297905d986e9470c2";
-      hash = "sha256-RXfOX3UJeoUub+/Xq4UnPEPJQq36aqQZNuIlor93xE4=";
-    };
-
-    meta = old.meta // {
-      platforms = prev.lib.platforms.linux;
-    };
-  });
+  auxves.checks = prev.auxves.checks or { } // prev.lib.internal.prefixAttrs "patches" { };
 }
